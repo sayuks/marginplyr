b82bd7b6649b59c1112852cb82d770d6b11b19ff
sayuks <sayuks.dev@gmail.com>
2025-03-24 17:29:45 +0900

    added test for .keep arg

--- tests/testthat/test-nest_by_with_margins.R
+++ tests/testthat/test-nest_by_with_margins.R
@@ -34,3 +34,35 @@ test_that("nest_by_with_margins() works correctly with local data", {
 
   expect_identical(actual, expected)
 })
+
+test_that(".keep works", {
+  data <- get_data_dummy()
+
+  actual <- nest_by_with_margins(
+    .data = data,
+    .margins = c(g2, g3),
+    .without_all = year,
+    .with_all = h1,
+    .key = "nested",
+    .keep = TRUE
+  )
+
+  expected <- union_all_with_margins(
+    .data = data,
+    .margins = c(g2, g3),
+    .without_all = year,
+    .with_all = h1
+  )
+
+  expected <- dplyr::nest_by(
+    .data = expected,
+    year, g2, g3, h1,
+    .key = "nested",
+    .keep = TRUE
+  )
+
+  class(expected$nested) <- "list"
+  attr(expected$nested, "ptype") <- NULL
+
+  expect_identical(actual, expected)
+})
--- tests/testthat/test-nest_with_margins.R
+++ tests/testthat/test-nest_with_margins.R
@@ -26,3 +26,49 @@ test_that("nest_with_margins() works correctly with local data", {
 
   expect_identical(actual, expected)
 })
+
+test_that(".keep works", {
+  data <- get_data_dummy()
+
+  actual <- nest_with_margins(
+    .data = data,
+    .margins = c(g2, g3),
+    .without_all = year,
+    .with_all = h1,
+    .key = "nested",
+    .keep = TRUE
+  )
+
+  expected <- union_all_with_margins(
+    .data = data,
+    .margins = c(g2, g3),
+    .without_all = year,
+    .with_all = h1
+  )
+
+  expected <- tidyr::nest(
+    .data = expected,
+    .by = c(year, g2, g3, h1),
+    .key = "nested"
+  )
+
+  expected <- dplyr::arrange(.data = expected, year, g2, g3, h1)
+
+  expected <- dplyr::mutate(
+    .data = dplyr::rowwise(expected),
+    nested = list({
+      res <- dplyr::mutate(
+        .data = nested,
+        dplyr::pick(!nested)
+      )
+      dplyr::relocate(
+        .data = res,
+        year, g2, g3, h1
+      )
+    })
+  )
+
+  expected <- dplyr::ungroup(expected)
+
+  expect_identical(actual, expected)
+})
